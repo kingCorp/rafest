3b82688ca72258f3dad4b1694cc905f52c6da3f2
James Attasiem <jattasiem@epohcj.com>
2020-03-09 17:09:53 +0100

    image fixed

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static('public'))
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
